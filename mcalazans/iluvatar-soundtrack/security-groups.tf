889909e8b8e278e12b39c4c954da6cfab34043a8
Mayko Calazans <maykocalazans@gmail.com>
2019-02-08 00:37:43 -0200

    Improving the SGs, covering the bastions permissions now

--- mcalazans/iluvatar-soundtrack/security-groups.tf
+++ mcalazans/iluvatar-soundtrack/security-groups.tf
@@ -68,3 +68,49 @@ resource "aws_security_group" "shadowfax_sg" {
     ]
   }
 }
+
+#Bastion Security group
+resource "aws_security_group" "bastion_sg" {
+  name = "${var.vpc_name}:bastion_sg"
+  description = "Allow traffic to bastion instances"
+  vpc_id = "${aws_vpc.main_vpc.id}"
+  ingress {
+    from_port = "${var.bastion_port}"
+    to_port = "${var.bastion_port}"
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.bastion_address_allowed}"
+    ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+}
+
+#Bastion client security group - Allow the instance receive ssh conections
+resource "aws_security_group" "bastion_client_sg" {
+  name = "${var.vpc_name}:bastion_client_sg"
+  description = "Allow traffic from bastion instances to worker instances"
+  vpc_id = "${aws_vpc.main_vpc.id}"
+  ingress {
+    from_port = "${var.bastion_port}"
+    to_port = "${var.bastion_port}"
+    protocol = "tcp"
+    security_groups = [
+      "${aws_security_group.bastion_sg.id}"
+    ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+}
--- mcalazans/iluvatar-soundtrack/variables-bastion.tf
+++ mcalazans/iluvatar-soundtrack/variables-bastion.tf
@@ -1,7 +1,7 @@
 # Worker - Instance EC2 specs
 variable "bastion_AMI" {
 	description = "The AMI that will be used as base for bastions instances"
-	default = "ami-05657f14bae0acdd1"
+	default = "ami-0fd836fce3d8d9e4c"
 }
 
 variable "bastion_type" {
@@ -12,4 +12,25 @@ variable "bastion_type" {
 variable "bastion_key_name" {
 	description = "The key that will be used to access the instances by SSH"
 	default = "key-of-barad-dur"
+}
+
+variable "bastion_port" {
+  description = "The port that arod are listening"
+  default = 22
+}
+
+variable "bastion_address_allowed" {
+  description = "The port address that can access the port 22"
+  default = "0.0.0.0/0"
+}
+
+# ASG Bastion specs
+variable "max_bastions" {
+	description = "The maximum number of bastion instances" 
+	default = "2"
+}
+
+variable "min_bastions" {
+	description = "The minimum number of bastion instances" 
+	default = "1"
 }
